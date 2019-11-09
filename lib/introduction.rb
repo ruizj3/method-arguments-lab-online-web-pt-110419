1a71c661dbc1da6b7db855e92285e81deb7d8858
Joseph Ruiz <ruizj3@uw.edu>
2019-11-09 19:31:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def introduction(name,language)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, lanugauge)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end 
+end
